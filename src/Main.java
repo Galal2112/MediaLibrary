1d1612a5c0e640c9dbf8e3d68c8164b71cc9c83e
Galal Ahmed <islam.galal212@gmail.com>
2020-10-14 22:20:58 +0200

    add model implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import mediaDB.InteractiveVideo;
+import model.InteractiveVideoImpl;
+
 public class Main {
     public static void main(String[] args) {
-
+        InteractiveVideo interactiveVideo = new InteractiveVideoImpl();
     }
 }
