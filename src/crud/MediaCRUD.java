040b8047e754339b70759725438257cfeb9cbda0
Galal Ahmed <islam.galal212@gmail.com>
2020-11-15 21:52:06 +0100

    add thread safety to media operations

--- src/crud/MediaCRUD.java
+++ src/crud/MediaCRUD.java
@@ -6,46 +6,83 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MediaCRUD implements CRUD<MediaContent> {
-
+    private final Lock lock = new ReentrantLock();
     private static final LinkedList<MediaContent> mediaList = new LinkedList<>();
 
     @Override
     public List<MediaContent> getAll() {
-        return new LinkedList<>(mediaList);
-    }
+        this.lock.lock();
 
-    @Override
-    public void create(MediaContent media) {
-        mediaList.add(media);
-    }
+        try {
+            return new LinkedList<>(mediaList);
 
-    @Override
-    public void update(MediaContent media) {
-        Iterator<MediaContent> it = mediaList.iterator();
-        int index = 0;
-        while (it.hasNext()) {
-            if (it.next().getAddress().equals(media.getAddress())) {
-                mediaList.set(index, media);
-                break;
-            }
-            index ++;
+        } finally {
+            this.lock.unlock();
         }
     }
 
     @Override
+    public void create(MediaContent media) {
+
+        this.lock.lock();
+
+        try {
+            mediaList.add(media);
+        } finally {
+            this.lock.unlock();
+        }
+    }
+
+    @Override
+    public void update(MediaContent media) {
+        this.lock.lock();
+
+        try {
+            Iterator<MediaContent> it = mediaList.iterator();
+            int index = 0;
+            while (it.hasNext()) {
+                if (it.next().getAddress().equals(media.getAddress())) {
+                    mediaList.set(index, media);
+                    break;
+                }
+                index++;
+            }
+        } finally {
+            this.lock.unlock();
+        }
+
+    }
+
+    @Override
     public Optional<MediaContent> get(String address) {
-        return mediaList.stream().filter(v -> v.getAddress().equals(address)).findFirst();
+        this.lock.lock();
+
+        try {
+            return mediaList.stream().filter(v -> v.getAddress().equals(address)).findFirst();
+
+        } finally {
+            this.lock.unlock();
+        }
     }
 
     @Override
     public void delete(MediaContent media) {
-        mediaList.removeIf( v -> v.getAddress().equals(media.getAddress()));
+        deleteById(media.getAddress());
     }
 
     @Override
     public void deleteById(String address) {
-        mediaList.removeIf(v -> v.getAddress().equals(address));
+        this.lock.lock();
+
+        try {
+            mediaList.removeIf(v -> v.getAddress().equals(address));
+
+        } finally {
+            this.lock.unlock();
+        }
     }
 }
